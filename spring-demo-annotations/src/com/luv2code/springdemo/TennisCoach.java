9fa2da4746102ff17873aba7c1847d3e4e8e38af
EdVoid <reploid_bass@hotmail.com>
2019-01-03 23:33:45 -0600

    loaded a properties file with fortunes

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @Scope("singleton")
 public class TennisCoach implements Coach {
 	@Autowired
-	@Qualifier("randomFortuneService")
+	@Qualifier("fileFortuneService")
 	private FortuneService fortuneService;
 	
 	public TennisCoach() {
