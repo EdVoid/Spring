30d7b0591ac0309a2ea2d87009f16bf2d749eda1
EdVoid <reploid_bass@hotmail.com>
2019-01-03 23:17:08 -0600

    postconstruct and predestroy

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,15 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("thatSillyCoach")
-@Scope("prototype")
+@Scope("singleton")
 public class TennisCoach implements Coach {
 	@Autowired
 	@Qualifier("randomFortuneService")
@@ -40,5 +43,18 @@ public class TennisCoach implements Coach {
 //		System.out.println(">> TennisCoach: inisde setFortuneService method");
 //		this.fortuneService=fortuneService;
 //	}
+	
+	
+	@PostConstruct
+	public void doInit() {
+		// some initializations
+		System.out.println(this.getClass().getSimpleName()+ " Bean initiated");
+	}
+	
+	@PreDestroy
+	public void doDestroy() {
+		// do before destroying the bean
+		System.out.println(this.getClass().getSimpleName()+" Bean is being destroyed");
+	}
            
 }
