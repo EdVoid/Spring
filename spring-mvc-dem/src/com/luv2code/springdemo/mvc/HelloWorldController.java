179389d2c0d6985b95d8adb6b1475a1437c6c905
EdVoid <reploid_bass@hotmail.com>
2019-02-25 20:58:03 -0600

    RequestParam annotation example

--- spring-mvc-dem/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-dem/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -35,6 +36,25 @@ public class HelloWorldController {
 		
 		// create the message
 		String result ="Hi! "+theName;
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		System.out.println("message to be printed:"+ result);
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName")String theName, Model model) {
+		
+		// read the request parameter from the HTML form
+		// replaced by @RequestParam
+//		String theName = (request.getParameter("studentName"));
+		
+		// convert the name to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result ="Hello my dude!!!"+theName;
 		// add message to the model
 		model.addAttribute("message", result);
 		
